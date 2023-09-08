45779cf785ffdd98bd479bb58a4bf9bd82f57202
Louis Rai <47311747+LouisRai@users.noreply.github.com>
2023-09-08 07:32:30 +0545

    Account
    Serializer and view (API) for Account including Login and Creating new account with email verification.

---
+++ serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from apps.account.models import User, UserProfile
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ("id", "username", "email", "password", "first_name", "last_name")
+        extra_kwargs = {"password": {"write_only": True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserProfile
+        fields = ("address", "phone_number", "about_me", "profile_picture", "resume")
